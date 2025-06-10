11d90ba15d1bb1a94ee01b0d4e9d7b2dded5f133
MikaTech-dev <verasamoma@gmail.com>
2025-06-10 15:37:34 +0100

    added errr handling for auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,18 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-// POST auth/signup
-router.post ("/signup", (req, res) => {
-    res.send ("Signup endpoint")
+router.post("/signup", (req, res) => {
+    const {email, password} = req.body
+    if (!email || !password) {
+        res.status(400)
+        res.send({mesage: "You didn't send an email or a password along with your request."})
+    }
+    res.send (`User with email: ${email}, created successfully 🎉`)
+    console.log("User registered...");
+    
+});
+
+router.get ("/login", (req, res)=> {
+    res.status(200).send ("Login endpoint")
 })
-
-// POST auth/login
-router.post ("/signup", (req, res) => {
-    res.send ("Login endpoint")
-})
-
-module.exports = router;
+module.exports = router
